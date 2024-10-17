e4f8aca3714e4217dbe461dda5337fead74d0e62
Udit Raval <157107304+VIIVIIIIX@users.noreply.github.com>
2024-10-17 17:19:31 +0530

    added comments for better understanding.

--- Alternative Queries/Draw The Triangle 1/query.sql
+++ Alternative Queries/Draw The Triangle 1/query.sql
@@ -1,9 +1,9 @@
 --MS SQL SERVER
 
-DECLARE @VAR INT
-SET @VAR = 20
+DECLARE @VAR INT;
+SET @VAR = 20;
 WHILE @VAR > 0
     BEGIN
-    PRINT(REPLICATE("* ", @VAR))
+    PRINT(REPLICATE("* ", @VAR)); -- The REPLICATE() function repeats a string a specified number of times.
     SET @VAR = @VAR - 1;
     END
